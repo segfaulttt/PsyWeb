30c5b4bf498b2cddcd5cb2688bf431ceb15b0d95
segfault <segfault418@gmail.com>
2026-07-05 12:32:57 +0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/psyweb/PsywebApplication.java
@@ -0,0 +1,13 @@
+package com.psyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsywebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psyweb/PsywebApplicationTests.java
@@ -0,0 +1,34 @@
+package com.psyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+class PsywebApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("psyweb")
+                    .withUsername("psyweb")
+                    .withPassword("psyweb");
+
+    @DynamicPropertySource
+    static void dbProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}

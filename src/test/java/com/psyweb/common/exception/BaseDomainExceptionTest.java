4f1fcb9cd5e55222524affd907fea27aba41a707
segfault <segfault418@gmail.com>
2026-09-03 00:08:33 +0300

    feat(common): add domain exception hierarchy

--- src/test/java/com/psyweb/common/exception/BaseDomainExceptionTest.java
+++ src/test/java/com/psyweb/common/exception/BaseDomainExceptionTest.java
@@ -17,26 +17,23 @@ public class BaseDomainExceptionTest {
 	@Test
 	public void shouldExposeCause() {
 		Throwable cause = new RuntimeException("Original error");
-		TestDomainException exception = new TestDomainException("TEST_ERROR", "Test_message", cause);
+		TestDomainException exception = new TestDomainException("TEST_ERROR", "Test message", cause);
 		assertSame(cause, exception.getCause());
 	}
 	
 	@Test
 	public void shouldImplementDomainExceptionContract() {
-		TestDomainException exception = new TestDomainException("TEST_ERROR", "Test_message");
+		TestDomainException exception = new TestDomainException("TEST_ERROR", "Test message");
 		assertInstanceOf(DomainException.class, exception);
 	}
 	
-	
-	
-	public static final class TestDomainException extends BaseDomainException{
-		protected TestDomainException(String code, String message) {
+	private static final class TestDomainException extends BaseDomainException {
+		private TestDomainException(String code, String message) {
 			super(code, message);
 		}
 		
-		protected TestDomainException(String code, String message, Throwable cause) {
+		private TestDomainException(String code, String message, Throwable cause) {
 			super(code, message, cause);
 		}
 	}
-	
 }
